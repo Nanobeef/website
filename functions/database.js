5ead00589a2471c211066829fc36135c06a4c0bd
IC Weiner <ic@Turing.lan>
2023-11-16 00:59:57 -0600

    Worker

--- functions/database.js
+++ functions/database.js
@@ -1,6 +1,25 @@
-export default {
-  async fetch(request, env, ctx) {
-	console.log(env.API_KEY);
-    return new Response(`API host: ${env.API_KEY}`);
+var worker_default = {
+  async fetch(request, env) {
+    const data = {
+      message: "Hello !",
+      number: 42,
+      float: 32.5556,
+      api: env.API_KEY,
+    };
+    const json = JSON.stringify(data, null, 2);
+    const headers = {
+      "content-type": "application/json;charset=UTF-8",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, HEAD, POST, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type"
+    };
+    return new Response(json, {
+      headers
+    });
   }
-}
+};
+export {
+  worker_default as default
+};
+//# sourceMappingURL=worker.js.map
+
