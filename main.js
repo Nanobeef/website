8e980866f956cd3296ace299e5da125a2af2ca26
IC Weiner <ic@Turing.lan>
2023-11-15 20:30:48 -0600

    Hello

--- main.js
+++ main.js
@@ -3,6 +3,7 @@
 function main(){
 
 	console.log("One");
-	document.getElementById("Output").innerText = "Hello, is this working?";
+	document.getElementById("Output").innerText = "Hello, is this working? 2";
 	console.log("Hello");
+	console.log(API_KEY);
 }
